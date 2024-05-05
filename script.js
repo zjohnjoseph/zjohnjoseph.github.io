634c75daf22f5ad9ec9e39d63ccf65a2ba627342
John Joseph <zjohnjoseph@gmail.com>
2024-05-05 14:24:14 -0400

    Added Project Section

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const seeMoreButton = document.querySelector('.btn-see-more');
+    const additionalProjects = document.querySelector('.wl-row-two');
+  
+    seeMoreButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      additionalProjects.classList.toggle('show');
+      seeMoreButton.textContent = additionalProjects.classList.contains('show') ? 'See Less' : 'See More';
+    });
+  });
+
+
+  
   
