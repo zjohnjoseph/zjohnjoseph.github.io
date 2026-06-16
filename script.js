57ac0ba3f1fc275a3c37e5a6914a6aa419de4665
John Joseph <zjohnjoseph@gmail.com>
2026-06-16 00:07:34 -0400

    Updated index.html

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-document.addEventListener("DOMContentLoaded", function() {
-    const seeMoreButton = document.querySelector('.btn-see-more');
-    const additionalProjects = document.querySelector('.wl-row-two');
-  
-    seeMoreButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      additionalProjects.classList.toggle('show');
-      seeMoreButton.textContent = additionalProjects.classList.contains('show') ? 'See Less' : 'See More';
-    });
-  }); 
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
